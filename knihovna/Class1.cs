15449be9be306d89faf920acee93844c6394ed30
mikolasekM <125461226+mikolasekM@users.noreply.github.com>
2023-05-31 13:17:00 +0200

    2.0

--- knihovna/Class1.cs
+++ knihovna/Class1.cs
@@ -22,7 +22,7 @@
         {
             Console.Clear();
             Console.WriteLine("zadej název knihy");
-            string jmenoKnihy= Console.ReadLine();
+            string jmenoKnihy = Console.ReadLine();
             Console.WriteLine("zadej autora knihy");
             string autorKnihy = Console.ReadLine();
             Console.WriteLine("zadej rok vydání knihy");
@@ -34,29 +34,55 @@
         {
             foreach(var kniha in list)
             {
-                Console.WriteLine(kniha.nazev);
+                Console.WriteLine($"Název: {kniha.nazev} od {kniha.autor}, vydáno {kniha.rok}");
             }
+            Console.ReadKey();
         }
+        public static void vyhledaniAutor()
+        {
+            Console.WriteLine("napiž jméno autora");
+            string autor = Console.ReadLine();
 
+            var nalezeneKnihy = list.FindAll(p => p.autor == autor);
+
+            foreach (var kniha in nalezeneKnihy)
+            {
+                Console.WriteLine($"Název: {kniha.nazev} od {kniha.autor}, vydáno {kniha.rok}");
+            }
+
+            Console.ReadKey();
+        }
     }
 
 
 
     public static void main() {
 
-        string vyber = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
-            .Title("Vyber:")
-            .AddChoices(new string[] {"Přidat knihu", "Zobrazit knihu"})
-        );
         
         while (true)
         {
+            Console.Clear();
+            string vyber = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Vyber:")
+                .AddChoices(new string[] {"Přidat knihu", "Zobrazit knihu", "Vyhledat knihu podle autora"})
+            );
             switch(vyber)
             {
                 case "Přidat knihu": {
                         KniHOVNA.PridejKnihu();
                 } break;
+
+                case "Zobrazit knihu":
+                    {
+                        KniHOVNA.vypisKnihy();
+                    }
+                break;
+
+                case "Vyhledat knihu podle autora":
+                    {
+                        KniHOVNA.vyhledaniAutor();
+                    } break;
             }
         }
     }
